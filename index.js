f279952c6281f23b7d0388d90e092053bb7e8af2
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-05-28 23:09:42 +0800

    learning about node and sequelize to create api

---
+++ index.js
@@ -0,0 +1,22 @@
+import { ApolloServer } from 'apollo-server';
+import typeDefs from './schema';
+import resolvers from './resolvers';
+import models from './models';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: {
+    endpoint: '/graphql',
+    settings: {
+      'editor.theme': 'light',
+    },
+  },
+});
+
+
+server.listen().then(({ url }) => {
+  models.sequelize.sync().then(() => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+});
---
+++ resolvers.js
@@ -0,0 +1,5 @@
+export default {
+  Query: {
+    h1: (parent, args, context, info) => 'hi',
+  },
+};
